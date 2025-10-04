d76cc352663b536ccd39a42ba5e606c98aee8795
moneer95 <mnyrskyk@gmail.com>
2025-10-04 10:50:27 +0300

    ft1

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ var server = http.createServer(function(req, res) { //create web server
     let body = '';
 
     if (req.method != 'POST') {
-      const url = new URL(req.url)
+      const url = new URL("https://takepayments.ea-dental.com" + req.url)
       console.log(url.searchParams.get('items'))
         // Return a form to collect payment details
         body = getPaymentForm();

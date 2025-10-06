e06373fb80b0762334fc32329ee0aaf368ed45fe
moneer95 <mnyrskyk@gmail.com>
2025-10-06 20:10:41 +0300

    workingt1

--- app.js
+++ app.js
@@ -164,6 +164,7 @@ var server = http.createServer(async function (req, res) { //create web server
         let reqFields = {
           action: 'SALE',
           merchantID: process.env.GATEWAY_MERCHANT_ID || '278346',
+          merchantSecret: process.env.GATEWAY_MERCHANT_SECRET || '5CZ4T3pdVLUN011UrKFD', // ✅ add this
           threeDSRef: session.threeDSRef,
           threeDSResponse: '',
         };
